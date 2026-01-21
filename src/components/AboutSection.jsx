8b8d758791d366f4d8ed763a323d93aaf9a4998b
codewithzilan <mohammadzilan31@gamil.com>
2026-01-21 07:07:10 +0600

    Fixed issues

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1xaJ1CcQ-02MhGKOMGKGn8oRmECgBSBzX/view"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
