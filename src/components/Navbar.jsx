6845725ca595383c053723cef49e88e81a64fa3a
codewithzilan <mohammadzilan31@gamil.com>
2026-01-27 00:35:17 +0600

    Changed Issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground">M🟣Z</span>{" "}
+            <span className="text-glow text-foreground">Mohammad Zilan</span>{" "}
           </span>
         </a>
 
